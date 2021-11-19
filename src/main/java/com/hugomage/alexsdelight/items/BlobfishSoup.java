39007e2d55852fdea7dad8bf2b6b302c8977c7fe
HugoMage <69723114+HugoMage@users.noreply.github.com>
2021-11-19 12:57:22 -0600

    fixed bowl

--- src/main/java/com/hugomage/alexsdelight/items/BlobfishSoup.java
+++ src/main/java/com/hugomage/alexsdelight/items/BlobfishSoup.java
@@ -11,10 +11,21 @@ public class BlobfishSoup extends Item {
                 .tab(ItemGroup.TAB_FOOD)
                 .fireResistant()
                 .food(new Food.Builder()
-                                .nutrition(5).saturationMod(4).build()
-        )
-
-
-        );
+                                .nutrition(5).saturationMod(4).build()));
     }
+
+public ItemStack finishUsingItem(ItemStack p_77654_1_, World p_77654_2_, LivingEntity p_77654_3_) {
+        if (p_77654_3_ instanceof ServerPlayerEntity) {
+            ServerPlayerEntity serverplayerentity = (ServerPlayerEntity)p_77654_3_;
+            CriteriaTriggers.CONSUME_ITEM.trigger(serverplayerentity, p_77654_1_);
+            serverplayerentity.awardStat(Stats.ITEM_USED.get(this));
+        }
+
+        if (p_77654_3_ instanceof PlayerEntity && !((PlayerEntity)p_77654_3_).abilities.instabuild) {
+            p_77654_1_.shrink(1);
+        }
+
+        return p_77654_1_.isEmpty() ? new ItemStack(Items.BOWL) : p_77654_1_;
+    }
+
 }
