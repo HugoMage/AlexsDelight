67da117cdbac3348298765c62b36f80797368895
HugoMage <69723114+HugoMage@users.noreply.github.com>
2021-11-19 12:57:41 -0600

    bowl

--- src/main/java/com/hugomage/alexsdelight/items/CheesePasta.java
+++ src/main/java/com/hugomage/alexsdelight/items/CheesePasta.java
@@ -17,4 +17,19 @@ public class CheesePasta extends Item {
 
         );
     }
+    
+    public ItemStack finishUsingItem(ItemStack p_77654_1_, World p_77654_2_, LivingEntity p_77654_3_) {
+        if (p_77654_3_ instanceof ServerPlayerEntity) {
+            ServerPlayerEntity serverplayerentity = (ServerPlayerEntity)p_77654_3_;
+            CriteriaTriggers.CONSUME_ITEM.trigger(serverplayerentity, p_77654_1_);
+            serverplayerentity.awardStat(Stats.ITEM_USED.get(this));
+        }
+
+        if (p_77654_3_ instanceof PlayerEntity && !((PlayerEntity)p_77654_3_).abilities.instabuild) {
+            p_77654_1_.shrink(1);
+        }
+
+        return p_77654_1_.isEmpty() ? new ItemStack(Items.BOWL) : p_77654_1_;
+    }
+
 }
