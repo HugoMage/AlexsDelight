55232338aea258a142e8eb39bcf3a92ce572cfde
HugoMage <69723114+HugoMage@users.noreply.github.com>
2021-11-18 21:55:13 -0600

    added some foods

--- src/main/java/com/hugomage/alexsdelight/ItemsRegistry.java
+++ src/main/java/com/hugomage/alexsdelight/ItemsRegistry.java
@@ -1,6 +1,10 @@
 package com.hugomage.alexsdelight;
 
 
+import com.hugomage.alexsdelight.items.BlobfishSoup;
+import com.hugomage.alexsdelight.items.MincedCookedKangaroo;
+import com.hugomage.alexsdelight.items.MincedKangaroo;
+import com.hugomage.alexsdelight.items.MungusGummie;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.RegistryObject;
@@ -23,6 +27,11 @@ public class ItemsRegistry {
 
 
     // Items
+    public static final RegistryObject<Item> MUNGUS_GUMMIE = ITEMS.register("mungus_gummie", MungusGummie::new);
+    public static final RegistryObject<Item> BLOBFISH_SOUP = ITEMS.register("blobfish_soup", BlobfishSoup::new);
+    public static final RegistryObject<Item> MINCED_KANGAROO = ITEMS.register("minced_raw_kangaroo", MincedKangaroo::new);
+    public static final RegistryObject<Item> MINCED_COOKED_KANGAROO = ITEMS.register("minced_cooked_kangaroo", MincedCookedKangaroo::new);
+
 
 
 
